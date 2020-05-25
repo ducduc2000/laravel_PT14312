65335f4cdb267409203a289e9fbdba3ff6a1efb1
ducduc2000 <50909094+ducduc2000@users.noreply.github.com>
2020-05-25 20:19:48 +0800

    update

---
+++ database/factories/BlogFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+
+$factory->define(Model::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence(),
+        'contnet' => $fake->realText(),
+        'image' => $fake->imageUrl(),
+        'uer_id' => $fake->numberBetween(1, 50),
+    ];
+});
